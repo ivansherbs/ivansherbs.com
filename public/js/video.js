4aa691200a2f93b780eb070427a838449b541d06
Gabriel Petrovay <gabipetrovay@gmail.com>
2018-05-16 19:44:18 +0200

    Initial commit

---
+++ public/js/video.js
@@ -0,0 +1,74 @@
+var tag = document.createElement('script');
+tag.id = 'video-script';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-iframe', {
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.setPlaybackRate(0.5);
+    player.mute();
+}
+
+function onPlayerStateChange(event) {
+    switch (event.data) {
+        case YT.PlayerState.PLAYING:
+            $('.page-loader').css({
+                opacity: 0,
+                visibility: 'hidden'
+            });
+            $('.fullscreen .container').css({
+                opacity: 1
+            });
+            break;
+        case YT.PlayerState.ENDED:
+            player.seekTo(0);
+            break;
+    }
+}
+
+$(document).ready(function () {
+	// video player initialization
+    updateVideoIframe();
+    $(window).resize(function() {
+        updateVideoIframe();
+    });
+
+	// fullPage.js initialization
+	$('#fullpage').fullpage();
+});
+
+function updateVideoIframe() {
+    $videoContainer = $('.video-container');
+    var contWidth = $videoContainer.width();
+    var contHeight = $videoContainer.height() + 50;
+    var ratio = 0.5625;
+
+	var width = 0;
+	var height = 0;
+    var margin = 0;
+
+    if (ratio < contHeight / contWidth) {
+        height = contHeight;
+        width = height / ratio;
+        margin = Math.round((width - contWidth) / 2);
+    } else {
+        width = contWidth;
+        height = width * ratio;
+    }
+
+	var $iframe = $('#video-iframe').css({
+		width: width + 'px',
+		height: height + 'px',
+        margin: '0 0 0 -' + margin + 'px'
+    });
+}
