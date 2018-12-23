7baf8601a9d7116d00b12dc2892ea75b4e3629fa
Gabriel Petrovay <gabipetrovay@gmail.com>
2018-12-23 00:38:56 +0000

    All changes since but with another user

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    res.render('index', { title: 'Express' });
+});
+
+/* POST pages. */
+router.post('/charge', (req, res, next) => {
+    console.dir(req.body);
+    res.redirect('thankyou');
 });
 
 module.exports = router;
