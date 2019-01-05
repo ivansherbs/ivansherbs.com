fb27b39522dfa88ea877e29ecd18eda8834abcb0
Gabriel Petrovay <gabipetrovay@gmail.com>
2019-01-05 16:32:43 +0000

    Migrated from static HTML to express-handlebars view framework

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 
+var meetRouter = require('./routes/meet');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var webRootDir = path.join(__dirname, 'public');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('html', exphbs({
+    extname: '.html',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(webRootDir, { 'extensions': ['html'] }));
+app.use(express.static(webRootDir));
 
+app.use('/meet', meetRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     next(createError(404));
 });
 
-// error handler
-app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 module.exports = app;
---
+++ routes/meet.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+//router.get('/', (req, res, next) => {
+//    res.render('index', {});
+//});
+//
+//router.get('*', function(req, res){
+//    res.status(404).render('index');
+//});
+
+module.exports = router;
