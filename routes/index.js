336f9e6c475e25763498b197e2b175b09c7fa0e0
Gabriel Petrovay <gabipetrovay@gmail.com>
2019-02-20 07:49:05 +0100

    Added basic text file-based subscribe functionality

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,33 @@ router.post('/subscribe', (req, res, next) => {
     if (email.length === 100) {
         // TODO add resources file for texts
         ERROR_EMAIL_ADDRESS_TOO_LONG = 'This email looks suspiciously long to us.<br>Please contact us in case you really want to add such a long email address.';
-        res.render('subscribe', { error: ERROR_EMAIL_ADDRESS_TOO_LONG } );
+        res.render('subscribe', {
+            layout: 'basic',
+            email: email,
+            error: ERROR_EMAIL_ADDRESS_TOO_LONG
+        });
+        return;
     }
 
     // TODO add validators library
-    if (!email.match)
-	subscribers.add(req.body.your_precious);
-    res.render('subscribe');
+    //if (!email.match...)
+
+	subscribers.add(req.body.your_precious, err => {
+        if (err) {
+            ERROR_SUBSCRIBE_ADD_FAILED = 'Something terribly wrong happened while trying to save your email address. Please try again or contact us at hello@ivansherbs.com.';
+            res.render('subscribe', {
+                layout: 'basic',
+                email: email,
+                error: ERROR_SUBSCRIBE_ADD_FAILED
+            });
+            return;
+        }
+
+        res.render('subscribe', {
+            layout: 'basic',
+            message: 'Thank you for your support! The Ivan Chai Stories will soon reach you.'
+        });
+    });
 });
 
 // GET: 404 error page
